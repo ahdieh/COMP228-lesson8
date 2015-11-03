1db738c9cf22fdfbb449ae3b7caa6b8388537d6f
ahdieh <aemadi@my.centennialcollege.ca>
2015-11-03 10:55:28 -0600

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,10 @@
+// DRIVER CLASS +++++++++++++++++++++++++++++++++++++++++++++++
+public class Program {
+
+	//MAIN STATIC 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
